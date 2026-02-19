49a3373f6f7721a98881bda7b6b71d43b5e6e081
Keerthana R <keerthana241495@gmail.com>
2026-02-19 17:36:48 +0530

    removed config

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { API_BASE_URL } from '../config';
 import './Auth.css';
 
 const Register = () => {
